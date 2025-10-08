acb9c659ae6a8f7859c957ba7dc98a7fb887f7ae
DZultra <DanielZ123@web.de>
2025-10-08 20:01:54 +0200

    Refactored ugly code to use Directions instead of multiple separate functions

--- src/main/java/net/dzultra/block/SpawnPillarBlock/PedestalRodShiftingLogic.java
+++ src/main/java/net/dzultra/block/SpawnPillarBlock/PedestalRodShiftingLogic.java
@@ -2,67 +2,61 @@ package net.dzultra.block.SpawnPillarBlock;
 
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 
-import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class PedestalRodShiftingLogic {
-    public static void shiftPedestalRods(ServerWorld world, SpawnPillarBlockEntity blockEntity, ArrayList<BlockPos> positiveXBlocks, ArrayList<BlockPos> negativeXBlocks, ArrayList<BlockPos> positiveZBlocks, ArrayList<BlockPos> negativeZBlocks, int spawnTickCounter, int pedestalRodShiftStart) {
+
+    public static void shiftPedestalRods(
+            ServerWorld world,
+            SpawnPillarBlockEntity blockEntity,
+            List<BlockPos> positiveXBlocks,
+            List<BlockPos> negativeXBlocks,
+            List<BlockPos> positiveZBlocks,
+            List<BlockPos> negativeZBlocks,
+            int spawnTickCounter,
+            int pedestalRodShiftStart
+    ) {
         int ticksSinceStart = spawnTickCounter - pedestalRodShiftStart;
 
-        // Sort so you don't override blocks which you want to shift after
-        positiveXBlocks.sort((a, b) -> Integer.compare(b.getX(), a.getX()));
-        negativeXBlocks.sort((a, b) -> Integer.compare(a.getX(), b.getX()));
-        positiveZBlocks.sort((a, b) -> Integer.compare(b.getZ(), a.getZ()));
-        negativeZBlocks.sort((a, b) -> Integer.compare(a.getZ(), b.getZ()));
-
         // Shift every 5 ticks
-        if (ticksSinceStart % 5 == 0) {
-            for (BlockPos pos : positiveXBlocks) {
-                shiftBlockPositiveX(world, pos.add(blockEntity.getPedestal_rods_offset(), 0, 0));
-            }
-            for (BlockPos pos : negativeXBlocks) {
-                shiftBlockNegativeX(world, pos.add(-blockEntity.getPedestal_rods_offset(), 0, 0));
-            }
-            for (BlockPos pos : positiveZBlocks) {
-                shiftBlockPositiveZ(world, pos.add(0, 0, blockEntity.getPedestal_rods_offset()));
-            }
-            for (BlockPos pos : negativeZBlocks) {
-                shiftBlockNegativeZ(world, pos.add(0, 0, -blockEntity.getPedestal_rods_offset()));
-            }
+        if (ticksSinceStart % 5 != 0) return;
 
-            blockEntity.incrementPedestal_rods_offset();
+        // Sort all directions properly to avoid overwriting blocks
+        positiveXBlocks.sort(Comparator.comparingInt(BlockPos::getX).reversed());
+        negativeXBlocks.sort(Comparator.comparingInt(BlockPos::getX));
+        positiveZBlocks.sort(Comparator.comparingInt(BlockPos::getZ).reversed());
+        negativeZBlocks.sort(Comparator.comparingInt(BlockPos::getZ));
 
-            if (blockEntity.getPedestal_rods_offset() == 5) {
-                blockEntity.setPedestal_rods_offset(0);
-            }
+        int offset = blockEntity.getPedestal_rods_offset();
+
+        // Shift in each direction
+        shiftBlocks(world, positiveXBlocks, Direction.EAST, offset, 0);
+        shiftBlocks(world, negativeXBlocks, Direction.WEST, -offset, 0);
+        shiftBlocks(world, positiveZBlocks, Direction.SOUTH, 0, offset);
+        shiftBlocks(world, negativeZBlocks, Direction.NORTH, 0, -offset);
+
+        // Update offset
+        blockEntity.incrementPedestal_rods_offset();
+        if (blockEntity.getPedestal_rods_offset() >= 5) {
+            blockEntity.setPedestal_rods_offset(0);
         }
     }
 
-    private static void shiftBlockPositiveX(ServerWorld world, BlockPos pos) {
-        var state = world.getBlockState(pos);
-        if (state.isAir()) return;
-        world.removeBlock(pos, false);
-        world.setBlockState(pos.east(), state);
+    private static void shiftBlocks(ServerWorld world, List<BlockPos> positions, Direction direction, int xOffset, int zOffset) {
+        for (BlockPos pos : positions) {
+            BlockPos shiftedPos = pos.add(xOffset, 0, zOffset);
+            shiftBlock(world, shiftedPos, direction);
+        }
     }
 
-    private static void shiftBlockNegativeX(ServerWorld world, BlockPos pos) {
+    private static void shiftBlock(ServerWorld world, BlockPos pos, Direction direction) {
         var state = world.getBlockState(pos);
         if (state.isAir()) return;
-        world.removeBlock(pos, false);
-        world.setBlockState(pos.west(), state);
-    }
 
-    private static void shiftBlockPositiveZ(ServerWorld world, BlockPos pos) {
-        var state = world.getBlockState(pos);
-        if (state.isAir()) return;
         world.removeBlock(pos, false);
-        world.setBlockState(pos.south(), state);
-    }
-
-    private static void shiftBlockNegativeZ(ServerWorld world, BlockPos pos) {
-        var state = world.getBlockState(pos);
-        if (state.isAir()) return;
-        world.removeBlock(pos, false);
-        world.setBlockState(pos.north(), state);
+        world.setBlockState(pos.offset(direction), state);
     }
 }
